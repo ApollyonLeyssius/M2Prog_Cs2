bc7b7b5d94eaeb55454a89a34bdd4c729e70cd20
Julian <38302@ma-web.nl>
2024-11-22 11:53:43 +0100

    twee

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Program program = new Program();
+            program.Run();
+
+            //program is het variable
+            //program is een progamma
+            //het heet program
+            //De return is run
         }
+
+        internal void Run()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma");
+        }
+
     }
 }
