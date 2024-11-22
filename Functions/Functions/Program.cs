68000cf1bbee6eb3b8ee94caa9a89058562f285b
Julian <38302@ma-web.nl>
2024-11-22 12:22:51 +0100

    derde

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -13,9 +13,29 @@
             //De return is run
         }
 
+        internal void vraag1()
+        {
+
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+
+        }
+
         internal void Run()
         {
-            Console.WriteLine("Dit is nu de start van mijn programma");
+            Console.WriteLine("...");
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            Console.WriteLine("I could last 100 hours");
+            Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+            Console.WriteLine("No");
+            Console.WriteLine("Have you ever tried talking to a animal?");
+            Console.WriteLine("Probably");
+            Console.WriteLine("What is the worst game you played but that you liked anyway?");
+            Console.WriteLine("Yakuza 3");
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            Console.WriteLine("Because it's easier to transport");
         }
 
     }
