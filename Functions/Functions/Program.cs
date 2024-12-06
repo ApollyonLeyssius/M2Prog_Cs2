1866cd619a2c825475c3a57ad8a2095c85b837a2
ApollyonLeyssius <38302@ma-web.nl>
2024-12-06 12:05:02 +0100

    tweedeopdracht

--- Functions/Functions/Program.cs
+++ Functions/Functions/Program.cs
@@ -1,42 +1,81 @@
-﻿namespace Functions
+﻿using System.Net.Sockets;
+
+namespace Functions
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Program program = new Program();
-            program.Run();
+            Program program = new Program();             //program is het variable program is een progamma
+            program.Run();             //het heet program De return is run
+            Program vraag = new Program();
+            vraag.vraag1();
+            vraag.vraag2();
+            vraag.vraag3();
+            vraag.vraag4();
+            vraag.vraag5();
+            vraag.vraag6();
 
-            //program is het variable
-            //program is een progamma
-            //het heet program
-            //De return is run
+            string antwoord6 = vraag6();
+            Console.WriteLine(antwoord6);
         }
 
-        internal void vraag1()
-        {
-
-            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
-            string antwoord = Console.ReadLine();
-
-            Console.WriteLine(antwoord);
-
-        }
 
         internal void Run()
         {
             Console.WriteLine("...");
-            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
-            Console.WriteLine("I could last 100 hours");
-            Console.WriteLine("Has someone caught you dancing in front of the mirror?");
-            Console.WriteLine("No");
-            Console.WriteLine("Have you ever tried talking to a animal?");
-            Console.WriteLine("Probably");
-            Console.WriteLine("What is the worst game you played but that you liked anyway?");
-            Console.WriteLine("Yakuza 3");
-            Console.WriteLine("Why do round pizzas come in square boxes?");
-            Console.WriteLine("Because it's easier to transport");
         }
 
+        internal string vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord1 = Console.ReadLine();
+
+            return antwoord1;
+        }
+
+        internal string vraag2()
+        {
+            Console.WriteLine("What would you do if you won a million dollars?");
+            string antwoord2 = Console.ReadLine();
+
+            return antwoord2;
+        }
+
+        internal string vraag3()
+        {
+            Console.WriteLine("Why do round pizzas come in square boxes?");
+            string antwoord3 = Console.ReadLine();
+
+            return antwoord3;
+        }
+
+        internal string vraag4()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord4 = Console.ReadLine();
+
+            return antwoord4;
+        }
+
+        internal string vraag5()
+        {
+            Console.WriteLine("What secret conspiracy would you like to actually start letting other people know ? ");
+            string antwoord5 = Console.ReadLine();
+
+            return antwoord5;
+        }
+
+        internal string vraag6()
+        {
+            Console.WriteLine("What would you call a male ladybug?");
+            string antwoord6 = Console.ReadLine();
+
+            return antwoord6;
+        }
+
+
     }
+
 }
